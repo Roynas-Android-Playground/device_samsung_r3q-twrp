69f3535b8557696a4c0cee8d0d04c4029223da7b
Simon1511 <me@simon1511.de>
2023-10-19 21:30:04 +0900

    r3q: Decryption bringup

--- device.mk
+++ device.mk
@@ -6,3 +6,16 @@
 #
 
 LOCAL_PATH := device/samsung/r3q
+
+# Encryption
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libion \
+    libandroidicu
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libandroidicu.so
